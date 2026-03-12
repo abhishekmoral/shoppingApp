49713782ebcb85ef903f74466fb1d2a25906fb6c
Abhishek Moral <abhisheksinghmoral@gmail.com>
2026-03-12 07:39:55 +0530

    Costom sizes

---
+++ lib/utils/constant/sizes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class USizes {
+  //Padding and margin sizes.
+  static const double xs = 4.0; // extra small
+  static const double sm = 8.0; // small
+  static const double md = 16.0; // medium
+  static const double lq = 4.0; // large
+  static const double xl = 4.0; // extra large
+  // Icon Sizes
+  static const double iconxs = 12.0;// extra small
+  static const double iconsm = 16.0;// small
+  // static const double xs = 4.0;
+  // static const double xs = 4.0;
+  // static const double xs = 4.0;
+  // static const double xs = 4.0;
+  // static const double xs = 4.0;
+  // static const double xs = 4.0;
+  // static const double xs = 4.0;
+  //
+}
