e4d655794b5db89b5825c1cff35963b67abad3e5
Abhishek Moral <abhisheksinghmoral@gmail.com>
2026-03-14 10:10:28 +0530

    Json

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mykart/my_app.dart';
 
 void main() {
+  /// widgets flutter Binding
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  /// Flutter Native Splash
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(const MyApp());
 }
-
-
-
