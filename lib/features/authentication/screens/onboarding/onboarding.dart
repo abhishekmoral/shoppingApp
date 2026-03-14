9c031850010ed2a7cdd1f1ffb94cca2d8638543b
Abhishek Moral <abhisheksinghmoral@gmail.com>
2026-03-14 15:06:22 +0530

    helper folder

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:mykart/utils/constant/images.dart';
+import 'package:mykart/utils/constant/sizes.dart';
+import 'package:mykart/utils/constant/texts.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -12,11 +14,22 @@ class OnboardingScreen extends StatelessWidget {
         children: [
           PageView(
             children: [
-              Column(
-                children: [
-                  Lottie.asset(UImages.onboarding1Animation),
-                  Text("",)
-                ],
+              Padding(
+                padding: const EdgeInsets.only(right: USizes.defaultSpace, left: USizes.defaultSpace,top:
+                ),
+                child: Column(
+                  children: [
+                    Lottie.asset(UImages.onboarding1Animation),
+                    Text(
+                      UTexts.onBoardingTitle1,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    Text(
+                      UTexts.onBoardingSubTitle1,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
               ),
               Lottie.asset(UImages.onboarding2Animation),
               Lottie.asset(UImages.onboarding3Animation),
